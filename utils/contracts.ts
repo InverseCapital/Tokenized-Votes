23215a1820cfc858997e9c2405e11c8be215826f
Mattia Pomelli <mattiapomelli@gmail.com>
2022-07-03 21:34:10 +0200

    Add utility functions to get contracts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -1,12 +1,35 @@
 import { Contract, ContractInterface, ethers } from "ethers";
 
 import { getProvider } from "./providers";
+import * as customBallotJson from "../artifacts/contracts/CustomBallot.sol/CustomBallot.json";
+import * as myTokenJson from "../artifacts/contracts/MyToken.sol/MyToken.json";
+import { CustomBallot, MyToken } from "../typechain";
 
 export function getContract<T extends Contract>(
   address: string,
   abi: ContractInterface,
-  signer: ethers.Signer
+  withSigner = true
 ) {
   const provider = getProvider();
-  return new Contract(address, abi, signer ?? provider) as T;
+
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || "");
+  const signer = wallet.connect(provider);
+
+  return new Contract(address, abi, withSigner ? signer : provider) as T;
+}
+
+export function getBallotContract(withSigner = true) {
+  return getContract<CustomBallot>(
+    process.env.BALLOT_CONTRACT_ADDRESS || "",
+    customBallotJson.abi,
+    withSigner
+  );
+}
+
+export function getTokenContract(withSigner = true) {
+  return getContract<MyToken>(
+    process.env.TOKEN_CONTRACT_ADDRESS || "",
+    myTokenJson.abi,
+    withSigner
+  );
 }
